ab338bd972a20e555c055a506692e29a348e2e09
jinimp <linjinquan26@gmail.com>
2019-02-28 20:21:23 +0800

    病人信息增加返回诊断结果字段

--- QAS/Patient/serializers.py
+++ QAS/Patient/serializers.py
@@ -8,12 +8,25 @@ from rest_framework import serializers
 from .models import Patient
 
 
-class PatientSerializer(serializers.ModelSerializer):
+class SCPatientSerializer(serializers.ModelSerializer):
     """查增"""
     report_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
     send_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S')
+    diagnose_label = serializers.CharField(read_only=True)
 
     class Meta:
         model = Patient
         fields = ('id', 'name', 'age', 'gender', 'specimen_source',
-                  'num_no', 'report_time', 'send_time')
+                  'num_no', 'report_time', 'send_time', 'diagnose_label')
+
+
+class UPatientSerializer(serializers.ModelSerializer):
+    """修改"""
+    report_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+    send_time = serializers.DateTimeField(format='%Y-%m-%d %H:%M:%S', read_only=True)
+    diagnose_label = serializers.CharField(read_only=True)
+
+    class Meta:
+        model = Patient
+        fields = ('id', 'name', 'age', 'gender', 'specimen_source',
+                  'num_no', 'report_time', 'send_time', 'diagnose_label')
--- QAS/Patient/views.py
+++ QAS/Patient/views.py
@@ -6,7 +6,7 @@ from rest_framework.generics import ListAPIView, ListCreateAPIView, GenericAPIVi
 from Patient.models import Patient
 from Case.models import Case
 from Image.models import Image
-from Patient.serializers import PatientSerializer
+from Patient.serializers import SCPatientSerializer, UPatientSerializer
 
 import logging
 logger = logging.getLogger('qas')
@@ -17,12 +17,15 @@ class SCPatientView(APIView):
     """
     get: 查询一条病人的记录
     post: 新增一条病人的记录
+    patch: 修改一条病人的记录
     """
 
     def get(self, request):
 
         # 获取查询字符串中的参数
-        img_id = request.GET.get('img_id')
+        img_id = request.GET.get('img_id', None)
+        if not img_id:
+            return Response(status=status.HTTP_400_BAD_REQUEST, data={'msg': '参数错误！'})
 
         # 根据大图id, 查询数据库
         try:
@@ -39,35 +42,53 @@ class SCPatientView(APIView):
             return Response(status=status.HTTP_200_OK, data={'msg': '没有该病人信息', 'res_info': None})
 
         # 序列化返回
-        ser = PatientSerializer(patient)
-        return Response(status=status.HTTP_200_OK, data={'res_info': ser.data})
+        ser = SCPatientSerializer(patient)
+        # 添加诊断结果返回
+        res_info = ser.data
+        res_info['diagnose_label'] = image.icase.diagnose_label
+
+        return Response(status=status.HTTP_200_OK, data={'res_info': res_info})
 
     def post(self, request):
 
-        # 不保存diagnose_label和img_id到数据库, 因为patient表不包含这两个字段
-        diagnose_label = request.data.pop('diagnose_label')
-        img_id = request.data.pop('img_id')
+        # 不保存diagnose_label/img_id到数据库, 因为patient表不包含这两个字段
+        try:
+            diagnose_label = request.data.pop('diagnose_label')
+            img_id = request.data.pop('img_id')
+        except Exception as e:
+            logger.error(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST, data='参数错误！')
 
         # 获取参数, 校验参数
-        serializer = PatientSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        ser = SCPatientSerializer(data=request.data)
+        ser.is_valid(raise_exception=True)
+        ser.save()
 
         # 同时将新建的病人信息id, 大图id保存到病例信息表
         try:
-            Case.objects.create(
+            case = Case.objects.create(
                 diagnose_label=diagnose_label,
                 image=Image.objects.get(id=img_id),
-                patient=Patient.objects.get(id=serializer.data['id'])
+                patient=Patient.objects.get(id=ser.data['id'])
             )
         except Exception as e:
             logger.error(e)
             return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR, data='数据库保存失败！')
 
-        # 返回结果
-        return Response(status=status.HTTP_201_CREATED, data=serializer.data)
+        # 添加诊断结果返回
+        res_info = ser.data
+        res_info['diagnose_label'] = case.diagnose_label
+
+        return Response(status=status.HTTP_201_CREATED, data={'res_info': res_info})
 
     def patch(self, request):
+
+        # 不保存diagnose_label/img_id到数据库, 因为patient表不包含这两个字段
+        try:
+            diagnose_label = request.data.pop('diagnose_label')
+        except Exception as e:
+            logger.error(e)
+            return Response(status=status.HTTP_400_BAD_REQUEST, data='参数错误！')
 
         # 根据id, 查询数据库对象
         try:
@@ -76,8 +97,19 @@ class SCPatientView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND, data={'msg': '数据不存在！'})
 
         # 获取参数, 校验参数, 保存结果
-        serializer = PatientSerializer(patient, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
+        ser = UPatientSerializer(patient, data=request.data)
+        ser.is_valid(raise_exception=True)
+        ser.save()
 
-        return Response(serializer.data)
+        # 通过病人反向查询其病例信息
+        case_id = patient.pcase.id
+        # 同时保存诊断结果到病例表中
+        case = Case.objects.get(id=case_id)
+        case.diagnose_label = diagnose_label
+        case.save()
+
+        # 添加诊断结果返回
+        res_info = ser.data
+        res_info['diagnose_label'] = case.diagnose_label
+
+        return Response(status=status.HTTP_200_OK, data={'res_info': res_info})
